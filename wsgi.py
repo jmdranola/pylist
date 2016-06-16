db7ce40de13ef09811323962405883a2f2690148
Jona Rañola (IS-PH) <jonar@ph.trendnet.org>
2016-06-16 13:41:47 +0800

    Added flask environment

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
@@ -8,3 +18,8 @@ from todopy import app as application
 #
 # Below for testing only
 #
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
