321722b343b0e7bb37eb26b7125cbbbd933456c6
Jona Rañola (IS-PH) <jonar@ph.trendnet.org>
2016-06-16 16:20:28 +0800

    updates

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ from app import app
 #conn = sqlite3.connect('magedb.db')
 
 @app.route('/')
-@app.route('/index')
+@app.route('/todolist')
 def todolist():
 	con = sql.connect("magedb.db")
 	con.row_factory = sql.Row
