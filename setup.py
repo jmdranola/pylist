2945ac6d10521ff692e8c8022dced099a054d516
Jona Rañola (IS-PH) <jonar@ph.trendnet.org>
2016-06-16 19:09:18 +0800

    Message

--- setup.py
+++ setup.py
@@ -8,3 +8,12 @@ setup(name='pylist',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask==0.7.2', 'MarkupSafe','Flask-SQLAlchemy==0.16']
      )
+
+BASE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+DATABASE_PATH = os.path.join(BASE_DIR, 'magedb.db')
+
+DATABASES = {
+    'default': {
+        'NAME': DATABASE_PATH,
+    }
